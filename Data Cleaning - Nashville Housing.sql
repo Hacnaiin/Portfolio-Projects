7d2cbbe4e820e71c60f5d1e4f607d8e471c280b4
Hasnain Ashraf <161204665+Hacnaiin@users.noreply.github.com>
2024-03-23 17:42:30 +0500

    Update Data Cleaning - Nashville Housing.sql

--- Data Cleaning - Nashville Housing.sql
+++ Data Cleaning - Nashville Housing.sql
@@ -1,218 +1,117 @@
+-- Data cleaning on Nashville Housing Dataset
 
---Data Cleaning in SQL using Nashville Housing Dataset
+-- Link to the dataset: https://www.kaggle.com/datasets/tmthyjames/nashville-housing-data
 
+-- Checking what does our Dataset contains
+select * from Nashville
 
+-- Selecting the unique rows from LandUse column
+select distinct LandUse from Nashville
+order by 1 
 
-Select *
-From NashvilleHousing
+-- Deleting unwanted columns
+alter table Nashville
+drop column Acreage, TaxDistrict, LandValue, BuildingValue, TotalValue, OwnerAddress, OwnerName
 
---------------------------------------------------------------------------------------------------------------------------
+-- Display the duplicate rows
+select n1.ParcelID, n1.LandUse, n1.PropertyAddress, n1.LegalReference
+from Nashville n1
+join Nashville n2 
+	on n1.ParcelID = n2.ParcelID
+	and n1.[UniqueID ] <> n2.[UniqueID ]
 
--- Standardize Date Format
+-- Checking the Row count before deleting the duplicates (result = 56477)
+select count(UniqueID) as TotalCount from Nashville
 
+-- Finding & Deleting the duplicate rows
+with RowNumCte as(
+select *, ROW_NUMBER() over ( partition by ParcelID, PropertyAddress, SalePrice,
+LegalReference, DateSold order by UniqueID) Row_Num
+from Nashville
+)
+select * from RowNumCte
+where Row_Num > 1
+order by PropertyAddress
 
-Select saleDateConverted, CONVERT(Date,SaleDate)
-FromNashvilleHousing
+with RowNumCte as(
+select *, ROW_NUMBER() over ( partition by ParcelID, PropertyAddress, SalePrice,
+LegalReference, DateSold order by UniqueID) Row_Num
+from Nashville
+)
+delete from RowNumCte
+where Row_Num > 1
 
-
-Update NashvilleHousing
-SET SaleDate = CONVERT(Date,SaleDate)
-
--- If it doesn't Update properly
-
-ALTER TABLE NashvilleHousing
-Add SaleDateConverted Date;
-
-Update NashvilleHousing
-SET SaleDateConverted = CONVERT(Date,SaleDate)
-
-
- --------------------------------------------------------------------------------------------------------------------------
-
--- Populate Property Address data
-
-Select *
-From NashvilleHousing
---Where PropertyAddress is null
-order by ParcelID
-
-
-
-Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
-From NashvilleHousing a
-JOIN NashvilleHousing b
-	on a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-Where a.PropertyAddress is null
-
-
-Update a
-SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
-From NashvilleHousing a
-JOIN NashvilleHousing b
-	on a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-Where a.PropertyAddress is null
+-- Checking the Row count after deleting the duplicates (result = 56374)
+select count(UniqueID) as TotalCount from Nashville
 
 
+-- Renaming rows with wrong names
+select distinct LandUse from Nashville order by 1
+
+update Nashville set LandUse ='VACANT RESIDENTIAL LAND' where LandUse = 'VACANT RES LAND'
+update Nashville set LandUse ='VACANT RESIDENTIAL LAND' where LandUse = 'VACANT RESIENTIAL LAND'
+
+select PropertyAddress from Nashville where LandUse = 'VACANT RESIDENTIAL LAND'
+
+-- Updating PropertyAddress
+update Nashville 
+set PropertyAddress = REPLACE(PropertyAddress,'NASHVILLE','')
+where PropertyAddress like '%NASHVILLE'
+
+update Nashville 
+set PropertyAddress = REPLACE(PropertyAddress,',','')
+where PropertyAddress like '%, '
 
 
---------------------------------------------------------------------------------------------------------------------------
+-- Convert the data type of SaleDate
+alter table nashville
+add DateSold date
 
--- Breaking out Address into Individual Columns (Address, City, State)
+update Nashville set DateSold = CONVERT(date, SaleDate)
+
+select DateSold from Nashville
+
+alter table nashville drop column SaleDate
 
 
-Select PropertyAddress
-From NashvilleHousing
---Where PropertyAddress is null
---order by ParcelID
+-- Replacing Y with Yes and N with No in SoldAsVacant column
+select distinct SoldAsVacant, COUNT(SoldAsVacant) as Total
+from Nashville
+group by SoldAsVacant
 
+
+update Nashville
+set SoldAsVacant = case when SoldAsVacant = 'Y' then 'Yes'
+		when SoldAsVacant = 'N' then 'No'
+		else SoldAsVacant
+		end
+from Nashville
+
+-- Finding & Populating Property Address where it's Null
+select n1.ParcelID, n1.PropertyAddress, n2.ParcelID, n2.PropertyAddress, 
+ISNULL(n1.PropertyAddress, n2.PropertyAddress)
+from Nashville n1
+join Nashville n2
+	on n1.ParcelID = n2.ParcelID
+	and n1.[UniqueID ] <> n2.[UniqueID ]
+where n1.PropertyAddress is null
+
+update n1
+set PropertyAddress = ISNULL(n1.PropertyAddress , n2.PropertyAddress)
+from Nashville n1
+join Nashville n2
+	on n1.ParcelID = n2.ParcelID
+	and n1.[UniqueID ] <> n2.[UniqueID ]
+where n1.PropertyAddress is null
+
+
+-- Breaking PropertyAddress into sub parts
 SELECT
 SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
-, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
-
-From NashvilleHousing
-
-
-ALTER TABLE NashvilleHousing
-Add PropertySplitAddress Nvarchar(255);
-
-Update NashvilleHousing
-SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
-
-
-ALTER TABLE NashvilleHousing
-Add PropertySplitCity Nvarchar(255);
-
-Update NashvilleHousing
-SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
-
-
-
-
-Select *
-From NashvilleHousing
-
-
-
-
-
-Select OwnerAddress
-From NashvilleHousing
-
-
-Select
-PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-From NashvilleHousing
-
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitAddress Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitCity Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitState Nvarchar(255);
-
-Update NashvilleHousing
-SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-
-
-
-Select *
-From NashvilleHousing
-
-
-
-
---------------------------------------------------------------------------------------------------------------------------
-
-
--- Change Y and N to Yes and No in "Sold as Vacant" field
-
-
-Select Distinct(SoldAsVacant), Count(SoldAsVacant)
-From NashvilleHousing
-Group by SoldAsVacant
-order by 2
-
-
-
-
-Select SoldAsVacant
-, CASE When SoldAsVacant = 'Y' THEN 'Yes'
-	   When SoldAsVacant = 'N' THEN 'No'
-	   ELSE SoldAsVacant
-	   END
-From NashvilleHousing
-
-
-Update NashvilleHousing
-SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
-	   When SoldAsVacant = 'N' THEN 'No'
-	   ELSE SoldAsVacant
-	   END
-
-
-
-
-
-
------------------------------------------------------------------------------------------------------------------------------------------------------------
-
--- Remove Duplicates
-
-WITH RowNumCTE AS(
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY
-					UniqueID
-					) row_num
-
-From NashvilleHousing
---order by ParcelID
-)
-Select *
-From RowNumCTE
-Where row_num > 1
-Order by PropertyAddress
-
-
-
-Select *
-From NashvilleHousing
-
-
-
-
----------------------------------------------------------------------------------------------------------
-
--- Delete Unused Columns
-
-
-
-Select *
-From NashvilleHousing
-
-
-ALTER TABLE NashvilleHousing
-DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1 , LEN(PropertyAddress)) as City
+from Nashville
+
+alter table Nashville
+add Address nvarchar(255),
+City nvarchar(255)
 
